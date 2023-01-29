ed9023e565f944164a2b33730c7d3962c8f8e64b
Lorenc326 <iLorenc326@gmail.com>
2023-01-29 02:03:27 +0200

    Init session with map load

---
+++ session/session.go
@@ -0,0 +1,23 @@
+package session
+
+import (
+	"rogue.game/maps"
+)
+
+type Session struct {
+	gamemap *maps.GameMap
+}
+
+func (s *Session) Init() {
+	s.gamemap = maps.Read("default")
+}
+
+func (s *Session) RenderUserVision() *maps.UserVision {
+	userMap := maps.UserVision{}
+	for i := 0; i < 11; i++ {
+		for j := 0; j < 11; j++ {
+			userMap[i][j] = (*s.gamemap)[i][j]
+		}
+	}
+	return &userMap
+}
